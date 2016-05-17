8bfac49ecb8012dd6071d7b93555b4bdee7c1d28
spkb7ue <spkb7ue@gmail.com>
2016-05-18 00:07:09 +0800

    clean up

--- parallel/ThreadManager.cpp
+++ parallel/ThreadManager.cpp
@@ -13,14 +13,14 @@ void ThreadManager::ThreadFunction()
 {
     while(true)
     {
-        if(m_exitThread)
-        {
-            break;
-        }
         if(m_executeCallback)
         {
             m_callback();
             m_executeCallback = false;
+        }
+        else if(m_exitThread && !m_executeCallback)
+        {
+            break;
         }
     }
 }
@@ -28,20 +28,22 @@ void ThreadManager::ThreadFunction()
 bool ThreadManager::RegisterCallbackExecutionRequest()
 {
     std::lock_guard<SpinLock> mutex(m_lock);
+    if(m_exitThread)
+    {
+        return false;
+    }
+
     while(m_executeCallback)
     {
         // Spin thread while the call_back is executing
     }
 
     m_executeCallback = true;
-    return !m_exitThread;
+    return true;
 }
 
 void ThreadManager::RegisterThreadTerminationRequest()
 {
     std::lock_guard<SpinLock> mutex(m_lock);
-    while(m_executeCallback)
-    {
-
-    }
+    m_exitFlag = true;
 }
