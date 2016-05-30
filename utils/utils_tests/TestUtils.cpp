cfa22fd6b56de3d8db5b5cea9d75849a0921127e
spkb7ue <spkb7ue@gmail.com>
2016-05-31 00:14:58 +0800

    back up

--- utils/utils_tests/TestUtils.cpp
+++ utils/utils_tests/TestUtils.cpp
@@ -5,7 +5,7 @@
 using namespace raccoon;
 
 // Tests that the Foo::Bar() method does Abc.
-BOOST_AUTO_TEST_CASE(TestUtils_TestCtor)
+BOOST_AUTO_TEST_CASE(TestUtils_TestBinaryRepresentation)
 {
     static_assert(get_bits<9>::bit_num == 1001, "Got a wrong binary representation");
     static_assert(Bin<9>::value == 1001, "Got a wrong binary representation");
