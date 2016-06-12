8576118627df7a6bb6e6d5a2b380a45e7a600ead
spkb7ue <spkb7ue@gmail.com>
2016-06-13 01:22:17 +0800

    made thread manager a template

--- parallel/ThreadManager.cpp
+++ parallel/ThreadManager.cpp
@@ -3,47 +3,5 @@
 
 using namespace raccoon;
 
-ThreadManager::ThreadManager(std::function<void()> callback):
-m_callback(callback),
-m_exitThread(false),
-m_executeCallback(false)
-{}
 
-void ThreadManager::ThreadFunction()
-{
-    while(true)
-    {
-        if(m_executeCallback)
-        {
-            m_callback();
-            m_executeCallback = false;
-        }
-        else if(m_exitThread && !m_executeCallback)
-        {
-            break;
-        }
-    }
-}
 
-bool ThreadManager::RegisterCallbackExecutionRequest()
-{
-    std::lock_guard<SpinLock> mutex(m_lock);
-    if(m_exitThread)
-    {
-        return false;
-    }
-
-    while(m_executeCallback)
-    {
-        // Spin thread while the call_back is executing
-    }
-
-    m_executeCallback = true;
-    return true;
-}
-
-void ThreadManager::RegisterThreadTerminationRequest()
-{
-    std::lock_guard<SpinLock> mutex(m_lock);
-    m_exitThread = true;
-}
--- parallel/ThreadManager.h
+++ parallel/ThreadManager.h
@@ -2,23 +2,72 @@
 #include <functional>
 #include <atomic>
 #include "SpinLock.h"
+#include <mutex>
 
 namespace raccoon
 {
+    template<typename LockingPolicy>
     class ThreadManager
     {
     public:
         ThreadManager(std::function<void()> callback);
-
         bool RegisterCallbackExecutionRequest();
         void RegisterThreadTerminationRequest();
 
     private:
-        SpinLock m_lock;
+        LockingPolicy m_lock;
         std::function<void()> m_callback;
         void ThreadFunction();
 
         std::atomic_bool m_exitThread;
         std::atomic_bool m_executeCallback;
     };
+
+    template<typename LockingPolicy>
+    ThreadManager<LockingPolicy>::ThreadManager(std::function<void()> callback):
+        m_callback(callback),
+        m_exitThread(false),
+        m_executeCallback(false){}
+
+    template<typename LockingPolicy>
+    void ThreadManager<LockingPolicy>::ThreadFunction()
+    {
+        while(true)
+        {
+            if(m_executeCallback)
+            {
+                m_callback();
+                m_executeCallback = false;
+            }
+            else if(m_exitThread && !m_executeCallback)
+            {
+                break;
+            }
+        }
+    }
+
+    template<typename LockingPolicy>
+    bool ThreadManager<LockingPolicy>::RegisterCallbackExecutionRequest()
+    {
+        std::lock_guard<SpinLock> mutex(m_lock);
+        if(m_exitThread)
+        {
+            return false;
+        }
+
+        while(m_executeCallback)
+        {
+            // Spin thread while the call_back is executing
+        }
+
+        m_executeCallback = true;
+        return true;
+    }
+
+    template<typename LockingPolicy>
+    void ThreadManager<LockingPolicy>::RegisterThreadTerminationRequest()
+    {
+        std::lock_guard<LockingPolicy> mutex(m_lock);
+        m_exitThread = true;
+    }
 }
